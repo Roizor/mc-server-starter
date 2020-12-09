fd0272ea3778b5ab31115ba488af58918f54c216
Roizor <68617740+Roizor@users.noreply.github.com>
2020-12-08 19:58:32 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+# how
+echo "Downloading BuildTools"
+mkdir /mcs
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+mv BuildTools.jar /mcs/BuildTools.jar
