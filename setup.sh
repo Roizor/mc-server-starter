e29f07d2de53ce9e3a14b92c8ea3cb8f89bc8e23
Roizor <68617740+Roizor@users.noreply.github.com>
2020-12-09 09:07:34 -0600

    Update and rename start.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ echo "Downloading BuildTools"
 mkdir /mcs
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 mv BuildTools.jar /mcs/BuildTools.jar
+# how
+echo 
+input -p "Minecraft Version: " mcv
